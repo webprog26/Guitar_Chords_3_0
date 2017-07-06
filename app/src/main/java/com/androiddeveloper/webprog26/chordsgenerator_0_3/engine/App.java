65b371a34006035aa1df5e1db14f39c69f1eb256
webprog26@gmail.com <webprog26@gmail.com>
2017-07-06 13:39:45 +0300

    Project created. Dependencies added

---
+++ app/src/main/java/com/androiddeveloper/webprog26/chordsgenerator_0_3/engine/App.java
@@ -0,0 +1,15 @@
+package com.androiddeveloper.webprog26.chordsgenerator_0_3.engine;
+
+import android.app.Application;
+
+/**
+ * Created by webpr on 06.07.2017.
+ */
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
