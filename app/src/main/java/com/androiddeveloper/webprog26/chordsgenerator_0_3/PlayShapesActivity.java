949911944e53e2d5c99de400ffe4c859b72f456a
webprog26 <webprog26@gmail.com>
2017-07-14 09:42:37 +0300

    PlayShapeActivity added

---
+++ app/src/main/java/com/androiddeveloper/webprog26/chordsgenerator_0_3/PlayShapesActivity.java
@@ -0,0 +1,19 @@
+package com.androiddeveloper.webprog26.chordsgenerator_0_3;
+
+import android.os.Bundle;
+
+
+
+public class PlayShapesActivity extends BaseActvity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_shapes);
+    }
+
+    @Override
+    protected void setupActivityComponent() {
+
+    }
+}
---
+++ app/src/main/java/com/androiddeveloper/webprog26/chordsgenerator_0_3/engine/mvp/interfaces/play_screen/PlayView.java
@@ -0,0 +1,8 @@
+package com.androiddeveloper.webprog26.chordsgenerator_0_3.engine.mvp.interfaces.play_screen;
+
+/**
+ * Created by webprog on 14.07.17.
+ */
+
+public interface PlayView {
+}
